48257b3da54f05d948bbe4358df9acb1f9f1ae0f
troias <hunter007nz@gmail.com>
2023-08-27 23:09:28 +1200

    added tanstack

---
+++ src/components/Events/NewEventsSection.jsx
@@ -0,0 +1,54 @@
+import { useQuery } from '@tanstack/react-query'
+
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import EventItem from './EventItem.jsx';
+import { fetchEvents } from '../../utils/http.js';
+
+
+export default function NewEventsSection() {
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['events'],
+    queryFn: fetchEvents,
+  }
+  );
+
+
+
+
+
+
+  let content
+
+  if (isLoading) {
+    content = <LoadingIndicator />;
+  }
+
+  if (error) {
+    content = (
+      <ErrorBlock title="An error occurred" message={error.message || 'Failed to fetch events.'} />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <section className="content-section" id="new-events-section">
+      <header>
+        <h2>Recently added events</h2>
+      </header>
+      {content}
+    </section>
+  );
+}
